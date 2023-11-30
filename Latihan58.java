8ffcc983f33ea8b6e9fd8bb1a438cbc3046be8dd
4NF0 <148747658+4NF0@users.noreply.github.com>
2023-11-30 11:37:29 +0700

    commit

---
+++ Latihan58.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan58;
+
+/**
+ *
+ * @author nm
+ * Nama : Adi Naufal F
+ * Nim : 22205050
+ * Kelas/Semester : TI/3
+ * Deskripsi Program : Program ini berisi tentang menghitung jumlah dan selisih pada suatu bilangan.
+ */
+public class Latihan58 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JumlahBilangan bil = new JumlahBilangan();
+       bil.tampilHasilJumlah();
+       
+       SelisihBilangan blg = new SelisihBilangan();
+       blg.tampilSelisih();
+    }
+    
+}
---
+++ SelisihBilangan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan58;
+
+/**
+ *
+ * @author nm
+ */
+public class SelisihBilangan extends Bilangan {
+    public void tampilSelisih(){
+        System.out.println("Hasil Selisih " +getX()+" - "+getY()+" = "
+                            +(getX()-getY()) );
+    }
+}
